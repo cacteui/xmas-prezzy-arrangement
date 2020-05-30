e0b03c0afa41d41c29278e5268e2bdf25d9cb4b4
Cacteui <bycacteui@gmail.com>
2020-05-30 10:58:56 +0200

    Create select of years to choose from

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,8 @@ import people from '../data/people.json';
 
 const elements = {
     staticList: document.querySelector('.list__static'),
-    dynamicList: document.querySelector('.list__dynamic')
+    dynamicList: document.querySelector('.list__dynamic'),
+    selectYear: document.querySelector('.select-year')
 };
 
 // Helper method to insert items to list
@@ -33,3 +34,15 @@ const insertDynamicList = () => {
     elements.dynamicList.innerHTML = createItemList(remainingItems, 'name');
 };
 insertDynamicList();
+
+// Create select of years
+const insertYearOptions = () => {
+    const currentYear = new Date().getFullYear();
+
+    for (let i = 0; i <= 10; i++) {
+        const year = currentYear + i;
+        const option = `<option value="${year}">${year}</option>`;
+        elements.selectYear.insertAdjacentHTML('beforeend', option);
+    }
+};
+insertYearOptions();
