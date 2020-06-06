087b61fa944c387b9822acb6033f5e43183f27d8
Cacteui <bycacteui@gmail.com>
2020-06-06 13:38:22 +0200

    Split up the event handler function

--- src/js/index.js
+++ src/js/index.js
@@ -34,35 +34,30 @@ const insertYearOptions = () => {
     }
 };
 
-// When selecting a year change the receiver list
-viewElements.selectYear.addEventListener('change', event => {
+const updateParticipantList = (selectedYear, participants) => {
     let list = ``;
-    const participants = state.participants;
     
     // Get the original year from data and the selected year to find the difference
-    const originalYear = state.data.originalYear;
-    const year = event.target.value;
-    const yearDifference = year - originalYear;
+    const yearDifference = selectedYear - state.data.originalYear;
 
     // A participant cannot give himself/herself a present - therefore we need to jump every year this will
     // happen, which is the participants array length - 1
     const amountOfJumps = Math.floor(yearDifference / (participants.length - 1));
-    
+
+    // Loop through the array to find the participant's reciever
     for (let participant of participants) {
         // Add the year difference to the receiverId to find the receiver for that year
         let receiverId = participant.givesPrezzyTo + yearDifference;
 
-        // To avoid the situation that a participant gives present to himself/herself
-        if (amountOfJumps > 0) {
-            receiverId = receiverId + amountOfJumps;
-        }
+        // Avoid the situation that a participant gives present to himself/herself
+        if (amountOfJumps > 0) receiverId = receiverId + amountOfJumps;
 
         // If the receiverId is bigger than the length of the participants array (and therefore bigger
-        // than any receiver IDs) the length should be subtracted to find the correct receivers participant ID
+        // than any receiver IDs) the length should be subtracted to find the correct receivers ID
         // If the receiverID is bigger than twice the array length this should be taken into account to
         if (receiverId > participants.length) {
             const numOfArrLengthsInReceiverId = Math.floor(receiverId / participants.length);
-            const remainder = receiverId % participants.length; // To check if the division is a clean one
+            const remainder = receiverId % participants.length; // To check if the division is clean
             
             // If the receiverId is twice or more the length of the array and it is NOT a clean division
             if (numOfArrLengthsInReceiverId >= 2 && remainder > 0) {
@@ -76,12 +71,20 @@ viewElements.selectYear.addEventListener('change', event => {
             }
         }
 
+        // Find the participant to receive present from the participant in the loop and add it to the list
         const receiver = participants.find(receiver => receiver.id === receiverId);
         list += `<li>${participant.name} gives present to ${receiver.name}</li>`;
     }
 
-    // Update the list on the UI
-    viewElements.giverList.innerHTML = list;
+    return list;
+};
+
+// When the select is changed
+viewElements.selectYear.addEventListener('change', event => {
+    
+    // Update the particpant list on the UI
+    const updatedParticpantList = updateParticipantList(event.target.value, state.data.participants)
+    viewElements.giverList.innerHTML = updatedParticpantList;
 });
 
 // The initial setup on runtime
